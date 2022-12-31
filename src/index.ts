5b5bd308fad740dbbd2f378c83fbb64e6fc04197
abdulqgg <abdul.gilani@gmail.com>
2022-12-31 23:32:00 +0000

    working migrate

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
 
-    // const post = orm.em.create(Post, {title: 'my first post'});
-    // await orm.em.persistAndFlush(post);
+    const post = orm.em.create(Post, {title: 'my first post'});
+    await orm.em.persistAndFlush(post);
 };
 
 main().catch((err) => {
     console.error(err);
 });
-
