97bf4575d0bf8a1ef679ef4daaa2e6340c47bd38
abdulqgg <abdul.gilani@gmail.com>
2023-01-01 00:29:15 +0000

    added apollo server with graphql

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,32 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
+    
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
-    const emFork = orm.em.fork();
-    const post = emFork.create(Post, { title: "my first post" });
+    const app = express();
+ 
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    });
 
-    await emFork.persistAndFlush(post);
+    await apolloServer.start();
 
-    const post1 = await orm.em.find(Post, {});
-    console.log(post1);
+    apolloServer.applyMiddleware({ app }):
+
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000')
+    })
 
 };
 
