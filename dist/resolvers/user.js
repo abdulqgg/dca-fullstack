f0ae05c1bd0cc189d3a1d9e01f1b346cdfbee6c6
abdulqgg <abdul.gilani@gmail.com>
2023-01-03 20:17:00 +0000

    argon2 hash passwords added

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -11,10 +11,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserResolver = void 0;
-const User_1 = require("src/entities/User");
+const User_1 = require("../entities/User");
 const type_graphql_1 = require("type-graphql");
+const argon2_1 = __importDefault(require("argon2"));
 let UsernamePasswordInput = class UsernamePasswordInput {
 };
 __decorate([
@@ -30,13 +34,17 @@ UsernamePasswordInput = __decorate([
 ], UsernamePasswordInput);
 let UserResolver = class UserResolver {
     async register(options, { em }) {
-        const user = em.create(User_1.User, { username: options.username });
+        const hashedPassword = await argon2_1.default.hash(options.password);
+        const user = em.create(User_1.User, {
+            username: options.username,
+            password: hashedPassword,
+        });
         await em.persistAndFlush(user);
         return user;
     }
 };
 __decorate([
-    (0, type_graphql_1.Mutation)(() => String),
+    (0, type_graphql_1.Mutation)(() => User_1.User),
     __param(0, (0, type_graphql_1.Arg)('options')),
     __param(1, (0, type_graphql_1.Ctx)()),
     __metadata("design:type", Function),
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,7 @@
-import { User } from "src/entities/User";
-import { MyContext } from "src/types";
+import { User } from "../entities/User";
+import { MyContext } from "../types";
 import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import argon2 from 'argon2';
 
 @InputType()
 class UsernamePasswordInput {
@@ -13,12 +14,16 @@ class UsernamePasswordInput {
 @Resolver()
 export class UserResolver {
 
-    @Mutation(() => String)
+    @Mutation(() => User)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() { em }: MyContext
     ) {
-        const user = em.create(User, { username: options.username })
+        const hashedPassword = await argon2.hash(options.password)
+        const user = em.create(User, { 
+            username: options.username,
+            password: hashedPassword,
+        })
         await em.persistAndFlush(user)
         return user;
     }
