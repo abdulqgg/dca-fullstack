841fc973115eae156031a5e90b1ed91cf6d7e402
abdulqgg <abdul.gilani@gmail.com>
2023-01-05 13:15:29 +0000

    cookie integration redis

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import session from "express-session";
+import { createClient } from "redis";
+import connectRedis from "connect-redis";
+import { MyContext } from "./types";
+
 
 const main = async () => {
     
@@ -16,12 +21,36 @@ const main = async () => {
 
     const app = express();
 
+    const RedisStore = connectRedis(session)
+    const redisClient = createClient({ legacyMode: true })
+    //redisClient.connect().catch(console.error)
+    
+    app.use(
+      session({
+        name: 'qid', // name for cookie
+        store: new RedisStore({ 
+            client: redisClient,
+            disableTTL: true,
+            disableTouch: true
+        }),
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+            httpOnly: true,
+            sameSite: 'lax',
+            secure: __prod__// cookie only works in https
+        },
+        saveUninitialized: false,
+        secret: "ihwkdhidsufdniek", // password for cookie
+        resave: false,
+      })
+    )
+
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
             resolvers: [HelloResolver, PostResolver, UserResolver],
             validate: false
         }),
-        context: () => ({ em: orm.em })
+        context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
     });
 
     await apolloServer.start();
